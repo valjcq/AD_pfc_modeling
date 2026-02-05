8874041b70314efc694e03cd3a177f44eae440aa
valjcq <valjcq@gmail.com>
2026-02-05 14:44:41 +0100

    ring attractor implementation

--- 2_state_code/Phi_input_output.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-
-def Phi_input_output(x, y, z, alpha, theta):
-    return 1 / (1 + np.exp(-alpha / (1 + z) * (x - (theta + y)))) - 1 / (
-        1 + np.exp(alpha * theta / (1 + z))
-    )
--- 2_state_code/k_input_output.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-
-def k_input_output(z, alpha, theta):
-    return np.exp(alpha * theta / (1 + z)) / (1 + np.exp(alpha * theta / (1 + z)))
